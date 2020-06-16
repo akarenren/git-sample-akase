f6421abe995296b9ca039887b79bf6be0ff34d30
akaseren <ren.akase@rakus.co.jp>
2020-06-16 11:42:57 +0900

    second commit

---
+++ src/main/java/com/example/Car.java
@@ -0,0 +1,21 @@
+package com.example;
+
+public class Car {
+
+	private String name;
+	private Integer speed;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getSpeed() {
+		return speed;
+	}
+	public void setSpeed(Integer speed) {
+		this.speed = speed;
+	}
+	
+	
+}
